de59c3474967e07dace7f66b14c79b32d0e2d83f
Anh Tuan <13tn204@gmail.com>
2025-04-17 20:58:39 +0700

    #31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 import "./style.css";
 
 const MyComponent = () => {
+  //   const tuna = "tuna"; //string
+  //   const tuna = 13; //number
+  //   const tuna = true; //boolean
+  //   const tuna = null; //null
+  //   const tuna = undefined; //undefined
+  const tuna = [1, 2, 3]; //array
+  //   const tuna = { name: "tuna", age: 13 }; //object
   return (
     <>
-      <div className="">Hello World!</div>
+      <div> Hello {JSON.stringify(tuna)}</div>
+      <div>{console.log("Tuna")};</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         ah
       </div>
