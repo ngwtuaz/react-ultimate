97b41df2db0d227873bb708f2689fe5306f2fd23
Anh Tuan <13tn204@gmail.com>
2025-04-19 14:45:21 +0700

    #36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,23 @@
 const TodoNew = (props) => {
   console.log("> check props: ", props);
   const { addNewTodo } = props;
-  addNewTodo("Tuan Nguyen");
+  // addNewTodo("Tuan Nguyen"); //fire
+  const handleClick = () => {
+    console.log("Click me"); //fire
+  };
+
+  const handleOnChange = (name) => {
+    console.log(name); //fire
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        onChange={(event) => handleOnChange(event.target.value)}
+        type="text"
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
