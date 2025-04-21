a0e8f68671c8efe017023385f0cb7e719eefd0ec
Anh Tuan <13tn204@gmail.com>
2025-04-21 15:53:20 +0700

    #52

---
+++ src/pages/book.jsx
@@ -0,0 +1,4 @@
+const BookPage = () => {
+  return <h1>Book Page</h1>;
+};
+export default BookPage;
--- src/pages/product.jsx
+++
@@ -1,4 +0,0 @@
-const ProductPage = () => {
-  return <h1>products Page</h1>;
-};
-export default ProductPage;
