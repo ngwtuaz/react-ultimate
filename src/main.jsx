cf32fa53c2bbf8a773bfe83f0b9b6e17181f2fc9
Anh Tuan <13tn204@gmail.com>
2025-04-21 14:50:04 +0700

    #45

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <h1>Login page </h1>,
+  },
+  {
+    path: "/register",
+    element: <h1>Register page</h1>,
+  },
+  {
+    path: "/users",
+    element: <h1>user page</h1>,
+  },
+  {
+    path: "/products",
+    element: <h1>products page</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
