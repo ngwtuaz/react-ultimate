bb95b03a7d6f18cda54eca9c2cd3986aa1ac2831
Anh Tuan <13tn204@gmail.com>
2025-04-22 21:14:29 +0700

    #67

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,4 +1,12 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UserPage = () => {
-  return <h1>Users Page</h1>;
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserForm />
+      <UserTable />
+    </div>
+  );
 };
 export default UserPage;
