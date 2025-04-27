d5495315070d473d89a8620edc907aca77942604
Anh Tuan <13tn204@gmail.com>
2025-04-27 16:26:00 +0700

    #82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
@@ -12,6 +12,7 @@ const ViewUserDetail = (props) => {
           setIsDetailOpen(false);
         }}
         open={isDetailOpen}
+        width={"40vw"}
       >
         {dataDetail ? (
           <>
@@ -22,6 +23,35 @@ const ViewUserDetail = (props) => {
             <p>Email: {dataDetail.email}</p>
             <br />
             <p>Phone number: {dataDetail.phone}</p>
+            <br />
+            <p>Avatar:</p>
+            <div>
+              <img
+                width={150}
+                height={100}
+                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                  dataDetail.avatar
+                }`}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="btnUpload"
+                style={{
+                  display: "block",
+                  width: "fit-content",
+                  marginTop: "15px",
+                  padding: "5px 10px",
+                  background: "orange",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+              >
+                Upload Avatar
+              </label>
+              <input type="file" hidden id="btnUpload" />
+            </div>
+            {/* <Button type="primary">Upload Avatar</Button> */}
           </>
         ) : (
           <>
