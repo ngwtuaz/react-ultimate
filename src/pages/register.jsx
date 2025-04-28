4eab7822e6ce7b52fc0c048d4cb21e36d99c1595
Anh Tuan <13tn204@gmail.com>
2025-04-28 21:47:28 +0700

    #89-uncontrolled components for register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,58 @@
+import { Button, Input, Form } from "antd";
+
 const RegisterPage = () => {
-  return <h1>Register Page</h1>;
+  const [form] = Form.useForm();
+  const onFinish = (value) => {
+    console.log(value);
+  };
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      name="basic"
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+    >
+      <div
+        style={{
+          margin: "50px",
+        }}
+      >
+        <Form.Item
+          label="Full Name"
+          name="fullName"
+          // rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          // rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          // rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Phone number"
+          name="phone"
+          // rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <div>
+          <Button onClick={() => form.submit()} type="primary">
+            Register
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
 };
 export default RegisterPage;
