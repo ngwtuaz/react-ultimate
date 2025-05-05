38c14be2bbe1c15b77467cc91cbe1997efa21478
Anh Tuan <13tn204@gmail.com>
2025-05-05 16:21:19 +0700

    #107-createbook_control-uncontrol

--- src/components/books/book.table.jsx
+++ src/components/books/book.table.jsx
@@ -1,26 +1,57 @@
-import { Table, Popconfirm, notification } from "antd";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import UpdateBookModal from "./update.book.modal";
-import { useState } from "react";
-import ViewUserDetail from "./view.book.detail";
-import { DeleteBookAPI } from "../../services/api.service";
+import { useEffect, useState } from "react";
+import { fetchAllBookAPI } from "../../services/api.service";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Button, Popconfirm, Table } from "antd";
+import BookDetail from "./book.detail";
+import CreateBookControl from "./create.book.control";
+import CreateBookUnControl from "./create.book.uncontrol";
 
-const BookTable = (props) => {
-  const {
-    dataBook,
-    loadBook,
-    current,
-    pageSize,
-    total,
-    setCurrent,
-    setPageSize,
-  } = props;
-  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
-  const [dataUpdate, setDataUpdate] = useState(null);
-  const [dataBookDetail, setDataBookDetail] = useState(null);
+const BookTable = () => {
+  const [dataBook, setDataBook] = useState([]);
+  const [current, setCurrent] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const [total, setTotal] = useState(0);
+
+  const [dataDetail, setDataDetail] = useState(null);
   const [isDetailOpen, setIsDetailOpen] = useState(false);
 
-  const columns = [
+  const [dataUpdate, setDataUpdate] = useState(null);
+  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+
+  useEffect(() => {
+    loadBook();
+  }, [current, pageSize]);
+
+  const loadBook = async () => {
+    const res = await fetchAllBookAPI(current, pageSize);
+    if (res.data) {
+      setDataBook(res.data.result);
+      setCurrent(res.data.meta.current);
+      setPageSize(res.data.meta.pageSize);
+      setTotal(res.data.meta.total);
+    }
+  };
+
+  const handleDeleteBook = async (id) => {
+    //a
+  };
+
+  const onChange = (pagination, filters, sorter, extra) => {
+    if (pagination && pagination.current) {
+      if (+pagination.current !== +current) {
+        setCurrent(+pagination.current);
+      }
+    }
+    if (pagination && pagination.pageSize) {
+      if (+pagination.pageSize !== +pageSize) {
+        setPageSize(+pagination.pageSize);
+      }
+    }
+  };
+
+  const column = [
     {
       title: "STT",
       render: (_, record, index) => {
@@ -28,19 +59,21 @@ const BookTable = (props) => {
       },
     },
     {
-      title: "ID",
+      title: "Id",
       dataIndex: "_id",
       render: (_, record) => {
         return (
-          <a
-            href="#"
-            onClick={() => {
-              setDataBookDetail(record);
-              setIsDetailOpen(true);
-            }}
-          >
-            {record._id}
-          </a>
+          <>
+            <a
+              href="#"
+              onClick={() => {
+                setDataDetail(record);
+                setIsDetailOpen(true);
+              }}
+            >
+              {record._id}
+            </a>
+          </>
         );
       },
     },
@@ -81,7 +114,7 @@ const BookTable = (props) => {
             />
           </a>
           <Popconfirm
-            title="Delete book"
+            title="Delete Book"
             description="Are you sure to delete this book?"
             onConfirm={() => handleDeleteBook(record._id)}
             okText="Yes"
@@ -95,47 +128,27 @@ const BookTable = (props) => {
     },
   ];
 
-  const handleDeleteBook = async (id) => {
-    const res = await DeleteBookAPI(id);
-    if (res.data) {
-      notification.success({
-        message: "Delete book",
-        description: `Delete book successfully`,
-      });
-      await loadBook();
-    } else {
-      notification.error({
-        message: "Delete book",
-        description: JSON.stringify(res.message),
-      });
-    }
-  };
-
-  const onChange = (pagination, filters, sorter, extra) => {
-    //neu thay doi trang: current
-    if (pagination && pagination.current) {
-      if (+pagination.current !== +current) {
-        setCurrent(+pagination.current); //"5" -> 5
-      }
-    }
-    // neu thay doi tong so phan tu: pageSize
-    if (pagination && pagination.pageSize) {
-      if (+pagination.pageSize !== +pageSize) {
-        setPageSize(+pagination.pageSize); //"5" -> 5
-      }
-    }
-  };
-
   return (
     <>
+      <div
+        style={{
+          marginTop: "10px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <h3>Table Book</h3>
+        <Button type="primary" onClick={() => setIsCreateOpen(true)}>
+          Create Book
+        </Button>
+      </div>
       <Table
-        columns={columns}
+        columns={column}
         dataSource={dataBook}
         rowKey={"_id"}
         pagination={{
           current: current,
           pageSize: pageSize,
-          showSizeChanger: true,
           total: total,
           showTotal: (total, range) => {
             return (
@@ -147,21 +160,24 @@ const BookTable = (props) => {
         }}
         onChange={onChange}
       />
-      <UpdateBookModal
-        isModalUpdateOpen={isModalUpdateOpen}
-        setIsModalUpdateOpen={setIsModalUpdateOpen}
-        dataUpdate={dataUpdate}
-        setDataUpdate={setDataUpdate}
-        loadBook={loadBook}
-      />
-      <ViewUserDetail
-        dataBookDetail={dataBookDetail}
-        setDataBookDetail={setDataBookDetail}
+      <BookDetail
+        dataDetail={dataDetail}
+        setDataDetail={setDataDetail}
         isDetailOpen={isDetailOpen}
         setIsDetailOpen={setIsDetailOpen}
+      />
+      {/* <CreateBookControl
+        isCreateOpen={isCreateOpen}
+        setIsCreateOpen={setIsCreateOpen}
+        loadBook={loadBook}
+      /> */}
+      <CreateBookUnControl
+        isCreateOpen={isCreateOpen}
+        setIsCreateOpen={setIsCreateOpen}
         loadBook={loadBook}
       />
     </>
   );
 };
+
 export default BookTable;
--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -1,43 +1,9 @@
-import BookForm from "../components/books/book.form";
 import BookTable from "../components/books/book.table";
-import { useEffect, useState } from "react";
-import { fetchAllBookAPI } from "../services/api.service";
 
 const BookPage = () => {
-  //lift-up state
-  const [dataBook, setDataBook] = useState([]);
-  const [current, setCurrent] = useState(1);
-  const [pageSize, setPageSize] = useState(5);
-  const [total, setTotal] = useState(0);
-
-  //empty array =>run once
-  //not empty => next value !== prev value
-  useEffect(() => {
-    loadBook();
-  }, [current, pageSize]); //[] + condition
-
-  const loadBook = async () => {
-    const res = await fetchAllBookAPI(current, pageSize);
-    if (res.data) {
-      setDataBook(res.data.result);
-      setCurrent(res.data.meta.current);
-      setPageSize(res.data.meta.pageSize);
-      setTotal(res.data.meta.total);
-    }
-  };
-
   return (
     <div style={{ padding: "20px" }}>
-      <BookForm loadBook={loadBook} />
-      <BookTable
-        dataBook={dataBook}
-        loadBook={loadBook}
-        current={current}
-        pageSize={pageSize}
-        total={total}
-        setCurrent={setCurrent}
-        setPageSize={setPageSize}
-      />
+      <BookTable />
     </div>
   );
 };
